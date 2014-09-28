28e1402ce6469521f4acd7eea66c1e9e7c6b9e78
Chris Ford <christophertford@gmail.com>
2014-09-28 20:53:28 +0100

    Initial release.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject leipzig/lein-template "0.1.0-SNAPSHOT"
+(defproject leipzig/lein-template "0.1.0"
   :description "A quickstart for Leipzig tracks."
   :url "https://github.com/ctford/leipzig-template"
-  :license {:name "MIT Licence" }
+  :license {:name "MIT Licence"}
   :eval-in-leiningen true)
