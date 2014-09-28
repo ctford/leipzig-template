175245733ca9e3ae6cfedec4c56ce63654deaf39
Chris Ford <christophertford@gmail.com>
2014-09-28 20:59:39 +0100

    Working towards 0.2.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject leipzig/lein-template "0.1.0"
+(defproject leipzig/lein-template "0.2.0-SNAPSHOT"
   :description "A quickstart for Leipzig tracks."
   :url "https://github.com/ctford/leipzig-template"
   :license {:name "MIT Licence"}
