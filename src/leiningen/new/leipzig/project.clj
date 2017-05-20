57dd2fb259538462081f5427a49fe635923e36a4
Chris Ford <christophertford@gmail.com>
2017-05-20 17:11:37 +0100

    Fix TieredStopAtLevel warning.

--- src/leiningen/new/leipzig/project.clj
+++ src/leiningen/new/leipzig/project.clj
@@ -1,5 +1,6 @@
 (defproject {{name}} "0.1.0-SNAPSHOT"
   :main ^{:skip-aot true} {{name}}.song
+  :jvm-opts ^:replace []
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [overtone "0.10.1"]
                  [leipzig "0.10.0"]])
